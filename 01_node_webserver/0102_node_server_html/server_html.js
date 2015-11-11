061b6d3d81d60748ef64832bb629e1c957f35207
Lyzzeth Parody Álvarez <lizparody@gmail.com>
2015-11-11 11:02:33 -0500

    added the html file to create server

---
+++ 01_node_webserver/0102_node_server_html/server_html.js
@@ -0,0 +1,12 @@
+var http = require('http');
+var myServer = http.createServer(function(request, response){
+  response.writeHead(200, {"Content-Type" : "text/html"});
+  response.write("i like <strong>programming</strong> and <strong>partying</strong>");
+  response.end();
+});
+
+myServer.listen(3000);
+
+//After you create the server, go to the terminal and type "node fileName".
+console.log("Go to http://localhost:3000 on your browser");
+//To exit the node mode, press ctrl c
