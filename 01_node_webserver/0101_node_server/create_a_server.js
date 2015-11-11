c58281f100eb4ef5a398c1bf2104a86a662ea389
Lyzzeth Parody Álvarez <lizparody@gmail.com>
2015-11-11 10:51:34 -0500

    added some expaination

--- 01_node_webserver/0101_node_server/create_a_server.js
+++ 01_node_webserver/0101_node_server/create_a_server.js
@@ -1,6 +1,6 @@
 var http = require('http');
 var myServer = http.createServer(function(request, response){
-  response.writeHead(200, {"Content-Type" : "text/html"});
+  response.writeHead(200, {"Content-Type" : "text/plain"});
   response.write("hello world");
   response.end();
 });
