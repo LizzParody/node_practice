f64bc4dd48f23509539b55272e3a4df5fa1b697f
Lyzzeth Parody Álvarez <lizparody@gmail.com>
2015-11-11 10:44:56 -0500

    added some text

--- 01_node_webserver/0101_node_server/create_a_server.js
+++ 01_node_webserver/0101_node_server/create_a_server.js
@@ -7,4 +7,6 @@ var myServer = http.createServer(function(request, response){
 
 myServer.listen(3000);
 
+//After you create the server, go to the terminal and type "node fileName".
 console.log("Go to http://localhost:3000 on your browser");
+//To exit the node mode, press ctrl c
